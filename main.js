953120d95045751a6ba641ed6a5d100bda7a6781
Julio Radilla <Itsdigitaljulio@gmail.com>
2023-02-07 21:33:47 -0600

    deleted media photos and pushed code

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const navBarLinks = document.querySelector('.headerList');
 toggleButton.addEventListener('click',() =>{
     navBarLinks.classList.toggle('active')
 })
+
+/*GOOGLE MAPS */
+//AIzaSyBtpuoLjdapdvFGMph_ogB-Eq4iGZjRLNM
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.847060, lng: -87.948630},
+      zoom: 8
+    });
+}
+window.onload = initMap; 
+/*GOOGLE MAPS */
